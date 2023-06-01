6fe767a3b5e49322e40cc85d9624dc7b72f2818c
dityaputrap <91526569+dityaputrap@users.noreply.github.com>
2023-06-01 21:46:04 +0700

    update login

--- application/views/login.php
+++ application/views/login.php
@@ -25,16 +25,16 @@
       <div class="hidden xl:flex flex-col min-h-screen">
         <a href="" class="-intro-x flex items-center pt-5">
           <img alt="Midone - HTML Admin Template" class="w-6" src="<?= site_url('asset') ?>/admin/dist/images/logo.svg">
-          <span class="text-white text-lg ml-3"> Icewall </span>
+          <span class="text-white text-lg ml-3"> EasyPickTrash </span>
         </a>
         <div class="my-auto">
-          <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="<?= site_url('asset') ?>/admin/dist/images/illustration.svg">
+          <!-- <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="<?= site_url('asset') ?>/admin/dist/images/illustration.svg"> -->
           <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">
-            A few more clicks to
+          Beberapa langkah lagi
             <br>
-            sign in to your account.
+            untuk masuk ke akun Anda.
           </div>
-          <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Manage all your e-commerce accounts in one place</div>
+          <!-- <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Kelola semua akun e-commerce Anda di satu tempat</div> -->
         </div>
       </div>
       <!-- END: Login Info -->
@@ -42,9 +42,9 @@
       <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
         <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
           <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">
-            Sign In
+            Masuk
           </h2>
-          <div class="intro-x mt-2 text-slate-400 xl:hidden text-center">A few more clicks to sign in to your account. Manage all your e-commerce accounts in one place</div>
+          <div class="intro-x mt-2 text-slate-400 xl:hidden text-center">Beberapa langkah lagi untuk masuk ke akun Anda.</div>
           <form action="<?= site_url('welcome') ?>" method="post">
             <div class="intro-x mt-8">
               <input type="text" class="intro-x login__input form-control py-3 px-4 block" name="email" required autocomplete="off" placeholder="Email">
@@ -53,15 +53,15 @@
             <div class="intro-x flex text-slate-600 dark:text-slate-500 text-xs sm:text-sm mt-4">
               <div class="flex items-center mr-auto">
                 <input id="remember-me" type="checkbox" class="form-check-input border mr-2">
-                <label class="cursor-pointer select-none" for="remember-me">Remember me</label>
+                <label class="cursor-pointer select-none" for="remember-me">Ingatkan saya</label>
               </div>
             </div>
             <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-              <button type="submit" class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Login</button>
-              <a href="<?= site_url('register') ?>" class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Register</a>
+              <button type="submit" class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Masuk</button>
+              <a href="<?= site_url('register') ?>" class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Daftar</a>
             </div>
           </form>
-          <div class="intro-x mt-10 xl:mt-24 text-slate-600 dark:text-slate-500 text-center xl:text-left"> By signin up, you agree to our <a class="text-primary dark:text-slate-200" href="">Terms and Conditions</a> & <a class="text-primary dark:text-slate-200" href="">Privacy Policy</a> </div>
+          <div class="intro-x mt-10 xl:mt-24 text-slate-600 dark:text-slate-500 text-center xl:text-left"> Dengan mendaftar, Anda menyetujui <a class="text-primary dark:text-slate-200" href="">Syarat dan Ketentuan</a> & <a class="text-primary dark:text-slate-200" href="">Privasi Kami</a> </div>
         </div>
       </div>
       <!-- END: Login Form -->

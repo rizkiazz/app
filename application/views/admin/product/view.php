97f7a30788583dee86014952aae48f735f9dc089
unknown <rizkiaziz124@gmail.com>
2023-06-27 08:28:05 +0700

    fix empty produk

--- application/views/admin/product/view.php
+++ application/views/admin/product/view.php
@@ -36,35 +36,46 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($product as $row) : ?>
-                        <tr class="intro-x">
-                            <td class="!py-4">
-                                <div class="flex items-center">
-                                    <div class="w-10 h-10 image-fit zoom-in">
-                                        <img alt="Midone - HTML Admin Template" class="rounded-lg border-1 border-white shadow-md tooltip" src="<?= base_url() . '/uploads/produk/' . $row->gambar ?>">
-                                    </div>
-                                    <a href="" class="font-medium whitespace-nowrap ml-4"><?= $row->kategori ?></a>
-                                </div>
-                            </td>
-                            <td class="whitespace-nowrap"> <a class="flex items-center underline decoration-dotted" href="javascript:;"><?= $row->nama_brg ?></a> </td>
-                            <td class="text-center">
-                                <div class="flex items-center">
-                                    <div class="text-xs text-slate-500 ml-1">( <?= $row->stok ?> ) </div>
-                                </div>
-                            </td>
-                            <td class="text-center whitespace-nowrap"><?= number_format($row->harga, 0, ',', '.') ?> poin</td>
-                            <td class="w-40">
-                                <div class="flex items-center justify-center text-success"> <i data-lucide="check-square" class="w-4 h-4 mr-2"></i> Active </div>
-                            </td>
-                            <td class="table-report__action w-56">
-                                <div class="flex justify-center items-center">
-                                    <a class="flex items-center mr-3" href="<?= site_url('admin/product/update/' . $row->id_brg) ?>"> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
-                                    <a class="flex items-center text-danger" href="<?= site_url('admin/product/delete/' . $row->id_brg) ?>" onclick="return confirm('Yakin Hapus')"> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
-                                </div>
+                    <?php if(empty($product)) { ?>
+                        <tr>
+                            <td colspan="12" class="text-center">
+                                <div> Produk Kosong </div>
                             </td>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
+                    <?php } else { ?>
+
+                        <?php foreach ($product as $row) : ?>
+                            
+                            <tr class="intro-x">
+                                <td class="!py-4">
+                                    <div class="flex items-center">
+                                        <div class="w-10 h-10 image-fit zoom-in">
+                                            <img alt="Midone - HTML Admin Template" class="rounded-lg border-1 border-white shadow-md tooltip" src="<?= base_url() . '/uploads/produk/' . $row->gambar ?>">
+                                        </div>
+                                        <a href="" class="font-medium whitespace-nowrap ml-4"><?= $row->kategori ?></a>
+                                    </div>
+                                </td>
+                                <td class="whitespace-nowrap"> <a class="flex items-center underline decoration-dotted" href="javascript:;"><?= $row->nama_brg ?></a> </td>
+                                <td class="text-center">
+                                    <div class="flex items-center">
+                                        <div class="text-xs text-slate-500 ml-1">( <?= $row->stok ?> ) </div>
+                                    </div>
+                                </td>
+                                <td class="text-center whitespace-nowrap"><?= number_format($row->harga, 0, ',', '.') ?> poin</td>
+                                <td class="w-40">
+                                    <div class="flex items-center justify-center text-success"> <i data-lucide="check-square" class="w-4 h-4 mr-2"></i> Active </div>
+                                </td>
+                                <td class="table-report__action w-56">
+                                    <div class="flex justify-center items-center">
+                                        <a class="flex items-center mr-3" href="<?= site_url('admin/product/update/' . $row->id_brg) ?>"> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
+                                        <a class="flex items-center text-danger" href="<?= site_url('admin/product/delete/' . $row->id_brg) ?>" onclick="return confirm('Yakin Hapus')"> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                    
+                    <?php } ?>
             </table>
         </div>
         <!-- END: Data List -->

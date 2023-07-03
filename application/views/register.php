72da652d7e19408fdfb78a6c91d61ec553cbb845
unknown <rizkiaziz124@gmail.com>
2023-07-03 19:28:33 +0700

    fix register form

--- application/views/register.php
+++ application/views/register.php
@@ -6,14 +6,33 @@
     <meta charset="utf-8">
     <link href="<?= site_url('asset') ?>/logo.png" rel="shortcut icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Icewall admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Icewall Admin Template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description"
+        content="Icewall admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
+    <meta name="keywords"
+        content="admin template, Icewall Admin Template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="LEFT4CODE">
     <title><?= $title ?> | EasyPickTrash</title>
     <!-- BEGIN: CSS Assets-->
     <link rel="stylesheet" href="<?= site_url('asset') ?>/admin/dist/css/app.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.css" rel="stylesheet">
     </link>
+    <style>
+    .password-strength-bar div {
+        background-color: #ddd;
+    }
+
+    .password-strength-bar .weak {
+        background-color: #ff4d4f;
+    }
+
+    .password-strength-bar .fair {
+        background-color: #ffad33;
+    }
+
+    .password-strength-bar .strong {
+        background-color: #52c41a;
+    }
+    </style>
     <!-- END: CSS Assets-->
 </head>
 <!-- END: Head -->
@@ -40,35 +59,45 @@
             <!-- END: Register Info -->
             <!-- BEGIN: Register Form -->
             <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
-                <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
+                <div
+                    class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
                     <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">
                         Daftar
                     </h2>
-                    <div class="intro-x mt-2 text-slate-400 dark:text-slate-400 xl:hidden text-center">Beberapa langkah lagi untuk masuk ke akun Anda.</div>
+                    <div class="intro-x mt-2 text-slate-400 dark:text-slate-400 xl:hidden text-center">Beberapa langkah
+                        lagi untuk masuk ke akun Anda.</div>
                     <form action="<?= site_url('register') ?>" method="post">
                         <div class="intro-x mt-8">
-                            <input type="text" class="intro-x login__input form-control py-3 px-4 block" placeholder="Nama Lengkap" name="nama_user" autocomplete="off">
+                            <input type="text" class="intro-x login__input form-control py-3 px-4 block"
+                                placeholder="Nama Lengkap" name="nama_user" autocomplete="off">
                             <?= form_error('nama_user', '<div class="text-danger small ml-2 mt-2">', '</div>') ?>
-                            <input type="email" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Email" name="email" autocomplete="off">
+                            <input type="email" class="intro-x login__input form-control py-3 px-4 block mt-4"
+                                placeholder="Email" name="email" autocomplete="off">
                             <?= form_error('email', '<div class="text-danger small ml-2 mt-2">', '</div>') ?>
-                            <input type="password" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Sandi" name="password_1" autocomplete="off">
+
+                            <input type="password" class="intro-x login__input form-control py-3 px-4 block mt-4"
+                                placeholder="Sandi" name="password_1" autocomplete="off">
                             <?= form_error('password_1', '<div class="text-danger small ml-2 mt-2">', '</div>') ?>
-                            <div class="intro-x w-full grid grid-cols-12 gap-4 h-1 mt-3">
-                                <div class="col-span-3 h-full rounded bg-success"></div>
-                                <div class="col-span-3 h-full rounded bg-success"></div>
-                                <div class="col-span-3 h-full rounded bg-success"></div>
-                                <div class="col-span-3 h-full rounded bg-slate-100 dark:bg-darkmode-800"></div>
+                            <div class="intro-x w-full grid grid-cols-12 gap-4 h-1 mt-3 password-strength-bar">
+                                <div class="col-span-4 h-full rounded"></div>
+                                <div class="col-span-4 h-full rounded"></div>
+                                <div class="col-span-4 h-full rounded"></div>
                             </div>
-                            <input type="password" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Konfirmasi Sandi" name="password_2" autocomplete="off">
+                            <div class="text-theme-9 mt-2 password-strength-label"></div>
+                            <input type="password" class="intro-x login__input form-control py-3 px-4 block mt-4"
+                                placeholder="Konfirmasi Sandi" name="password_2" autocomplete="off">
                         </div>
-                        <div class="intro-x flex items-center text-slate-600 dark:text-slate-500 mt-4 text-xs sm:text-sm">
+                        <div
+                            class="intro-x flex items-center text-slate-600 dark:text-slate-500 mt-4 text-xs sm:text-sm">
                             <input id="remember-me" type="checkbox" class="form-check-input border mr-2">
                             <label class="cursor-pointer select-none" for="remember-me">Saya setuju dengan</label>
                             <a class="text-primary dark:text-slate-200 ml-1" href="">Kebijakan Privasi</a>.
                         </div>
                         <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-                            <button type="submit" class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Daftar</button>
-                            <a href="<?= site_url('welcome') ?>" class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Masuk</a>
+                            <button type="submit"
+                                class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Daftar</button>
+                            <a href="<?= site_url('welcome') ?>"
+                                class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Masuk</a>
                         </div>
                     </form>
                 </div>
@@ -84,13 +113,78 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.js"></script>
 
     <?php if (@$_SESSION['sukses']) { ?>
-        <script>
-            swal("Good job!", "<?php echo $_SESSION['sukses']; ?>", "success");
-        </script>
-        <!-- jangan lupa untuk menambahkan unset agar sweet alert tidak muncul lagi saat di refresh -->
+    <script>
+    swal("Good job!", "<?php echo $_SESSION['sukses']; ?>", "success");
+    </script>
+    <!-- jangan lupa untuk menambahkan unset agar sweet alert tidak muncul lagi saat di refresh -->
     <?php unset($_SESSION['sukses']);
     } ?>
     <!-- END: JS Assets-->
+    <script>
+    $(document).ready(function() {
+        var passwordInput = $('input[name="password_1"]');
+        var strengthBar = $('.password-strength-bar');
+        var strengthLabel = $('.password-strength-label');
+
+        passwordInput.on('input', function() {
+            var password = $(this).val();
+            var strength = calculatePasswordStrength(password);
+
+            updateStrengthBar(strength);
+            updateStrengthLabel(strength);
+        });
+
+        function calculatePasswordStrength(password) {
+            var strength = 0;
+
+            if (password.length >= 8) {
+                strength++;
+            }
+
+            if (/[a-z]/.test(password)) {
+                strength++;
+            }
+
+            if (/[A-Z]/.test(password)) {
+                strength++;
+            }
+
+            if (/[0-9]/.test(password)) {
+                strength++;
+            }
+
+            return strength;
+        }
+
+        function updateStrengthBar(strength) {
+            strengthBar.find('div').removeClass('weak fair strong');
+            for (var i = 0; i < strength; i++) {
+                strengthBar.find('div').eq(i).addClass('strong');
+            }
+            for (var j = strength; j < 4; j++) {
+                strengthBar.find('div').eq(j).addClass('weak');
+            }
+        }
+
+        function updateStrengthLabel(strength) {
+            var label = '';
+            switch (strength) {
+                case 0:
+                case 1:
+                    label = 'Password Lemah';
+                    break;
+                case 2:
+                    label = 'Password Sedang';
+                    break;
+                case 3:
+                case 4:
+                    label = 'Password Kuat';
+                    break;
+            }
+            strengthLabel.text(label);
+        }
+    });
+    </script>
 </body>
 
 </html>

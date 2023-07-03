67271a4565ba65de44c5a0e3afd8d1c7e84e6702
unknown <rizkiaziz124@gmail.com>
2023-07-03 19:41:08 +0700

    fix detail product

--- application/views/detail_product.php
+++ application/views/detail_product.php
@@ -15,9 +15,13 @@
                    <div class="flex items-center border-b border-slate-200/60 dark:border-darkmode-400 pb-5 mb-5">
                        <div class="font-medium text-base truncate">Judul Produk</div>
                    </div>
-                   <div class="flex items-center"> <i data-lucide="clipboard" class="w-4 h-4 text-slate-500 mr-2"></i> <b>Nama Produk :</b> <a href="" class="underline decoration-dotted ml-1"> <?= $row->nama_brg ?></a> </div>
-                   <div class="flex items-center mt-3"> <i data-lucide="info" class="w-4 h-4 text-slate-500 mr-2"></i> <b>Deskripsi: &nbsp;</b>
-                     <?= $row->keterangan ?>
+                   <div class="flex items-center"> <i data-lucide="clipboard" class="w-4 h-4 text-slate-500 mr-2"></i>
+                       <b>Nama Produk :</b> <a href="" class="underline decoration-dotted ml-1">
+                           <?= $row->nama_brg ?></a>
+                   </div>
+                   <div class="flex items-center mt-3"> <i data-lucide="info" class="w-4 h-4 text-slate-500 mr-2"></i>
+                       <b>Deskripsi: &nbsp;</b>
+                       <?= $row->keterangan ?>
                    </div>
                </div>
            </div>
@@ -32,28 +36,30 @@
                                <tr>
                                    <th class="whitespace-nowrap !py-5">Produk Item</th>
                                    <th>Poin</th>
-                                   <!-- <th>Qty</th> -->
+                                   <th>Harga</th>
                                    <th>Kategori</th>
                                </tr>
                            </thead>
                            <tbody>
-                                   <tr>
-                                       <td class="!py-4">
-                                           <div class="flex items-center">
-                                            <img src="<?= base_url() . '/uploads/produk/' . $row->gambar ?>" width="40px">
-                                               <a href="" class="font-medium whitespace-nowrap ml-4"><?= $row->nama_brg ?></a>
-                                           </div>
-                                       </td>
-                                       <td><?= number_format($row->harga, 0, ',', '.') ?> poin</td>
-                                       <!-- <td><?= number_format($row->stok, 0, ',', '.') ?> Item</td> -->
-                                       <td><?= $row->kategori ?></td>
-                                   </tr>
+                               <tr>
+                                   <td class="!py-4">
+                                       <div class="flex items-center">
+                                           <img src="<?= base_url() . '/uploads/produk/' . $row->gambar ?>"
+                                               width="40px">
+                                           <a href=""
+                                               class="font-medium whitespace-nowrap ml-4"><?= $row->nama_brg ?></a>
+                                       </div>
+                                   </td>
+                                   <td><?= number_format($row->poin, 0, ',', '.') ?> Poin</td>
+                                   <td><?= number_format($row->harga, 0, ',', '.') ?> /kg,-</td>
+                                   <td><?= $row->kategori ?></td>
+                               </tr>
                            </tbody>
                        </table>
                    </div>
                </div>
            </div>
        </div>
-   <?php endforeach; ?>
+       <?php endforeach; ?>
        <!-- END: Transaction Details -->
    </div>

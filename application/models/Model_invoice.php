82e59cad95c5825b7bd83a08518bbcd87eb8157d
unknown <rizkiaziz124@gmail.com>
2023-07-04 21:27:41 +0700

    fix drop off

--- application/models/Model_invoice.php
+++ application/models/Model_invoice.php
@@ -3,60 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Model_invoice extends CI_Model
 {
-	// public function index()
-	// {
-	// 	date_default_timezone_set('Asia/Jakarta');
-	// 	$order_id = $this->input->post('order_id');
-	// 	$id_user = $this->input->post('id_user');
-	// 	$name = $this->input->post('name');
-	// 	$alamat = $this->input->post('alamat');
-	// 	$city = $this->input->post('kota');
-	// 	$kode_pos = $this->input->post('kode_pos');
-	// 	$payment_method = $this->input->post('payment_method');
-	// 	$ekspedisi = $this->input->post('ekspedisi');
-	// 	$mobile_phone = $this->input->post('mobile_phone');
-	// 	$tracking_id = $this->input->post('tracking_id');
-	// 	$email = $this->input->post('email');
-	// 	$status = $this->input->post('status');
-
-	// 	$invoice = array (
-	// 		'order_id' 			=> $order_id,
-	// 		'id_user' 			=> $id_user,
-	// 		'name' 				=> $name,
-	// 		'alamat' 			=> $alamat,
-	// 		'city' 				=> $city,
-	// 		'kode_pos' 			=> $kode_pos,
-	// 		'payment_method' 	=> $payment_method,
-	// 		'ekspedisi' 		=> $ekspedisi,
-	// 		'mobile_phone' 		=> $mobile_phone,
-	// 		'tracking_id' 		=> $tracking_id,
-	// 		'email' 			=> $email,
-	// 		'status' 			=> $status,
-	// 		'transaction_time' 	=> date('Y-m-d H:i:s'),
-	// 		'payment_limit' 	=> date('Y-m-d H:i:s', mktime( date('H'), date('i'), date('s'), date('m'), date('d') + 1, date('Y'))),
-	// 	);
-
-	// 	$this->db->insert('transaction', $invoice);
-	// 	$id_invoice = $this->db->insert_id();
-
-	// 	foreach ($this->cart->contents() as $item) {
-	// 		$data = array (
-	// 			'id_invoice' 	=> $order_id,
-	// 			'id_user' 		=> $id_user,
-	// 			'id_brg' 		=> $item['id'],
-	// 			'nama_brg' 		=> $item['name'],
-	// 			'jumlah' 		=> $item['qty'],
-	// 			'harga' 		=> $item['price'],
-	// 		);
-
-	// 		$this->db->insert('cart', $data);
-
-	// 	}
-
-	// 	return TRUE;
-
-	// }
-
 	public function pick_up()
 	{
 		date_default_timezone_set('Asia/Jakarta');
@@ -125,7 +71,7 @@ class Model_invoice extends CI_Model
 			'transaction_time' 	=> date('Y-m-d H:i:s'),
 			'payment_limit' 	=> date('Y-m-d H:i:s', mktime( date('H'), date('i'), date('s'), date('m'), date('d') + 1, date('Y'))),
 		);
-						die(var_dump($invoice));
+						// die(var_dump($invoice));
 
 
 		$this->db->insert('transaction', $invoice);

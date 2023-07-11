2e57a1c2f681453f21815324359f01e6e138766a
unknown <rizkiaziz124@gmail.com>
2023-07-11 13:10:35 +0700

    update view produk

--- application/views/admin/product/view.php
+++ application/views/admin/product/view.php
@@ -18,7 +18,8 @@
             </div>
             <div class="hidden xl:block mx-auto text-slate-500"></div>
             <div class="w-full xl:w-auto flex flex-wrap xl:flex-nowrap items-center gap-y-3 mt-3 xl:mt-0">
-                <a href="<?= site_url('admin/product/add') ?>" class="btn btn-primary shadow-md mr-2"> Tambah Produk Baru <i data-lucide="plus" class="w-4 h-4"></i></a>
+                <a href="<?= site_url('admin/product/add') ?>" class="btn btn-primary shadow-md mr-2"> Tambah Produk
+                    Baru <i data-lucide="plus" class="w-4 h-4"></i></a>
 
             </div>
         </div>
@@ -32,54 +33,65 @@
                         <th class="whitespace-nowrap">Stok</th>
                         <th class="whitespace-nowrap">Harga</th>
                         <th class="text-center whitespace-nowrap">Poin</th>
-                        <th class="text-center whitespace-nowrap">Status</th>
+                        <!-- <th class="text-center whitespace-nowrap">Status</th> -->
                         <th class="text-center whitespace-nowrap">ACTIONS</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php if(empty($product)) { ?>
-                        <tr>
-                            <td colspan="12" class="text-center">
-                                <div> Produk Kosong </div>
-                            </td>
-                        </tr>
+                    <tr>
+                        <td colspan="12" class="text-center">
+                            <div> Produk Kosong </div>
+                        </td>
+                    </tr>
                     <?php } else { ?>
 
-                        <?php foreach ($product as $row) : ?>
-                            
-                            <tr class="intro-x">
-                                <td class="!py-4">
-                                    <div class="flex items-center">
-                                        <div class="w-10 h-10 image-fit zoom-in">
-                                            <img alt="Midone - HTML Admin Template" class="rounded-lg border-1 border-white shadow-md tooltip" src="<?= base_url() . '/uploads/produk/' . $row->gambar ?>">
-                                        </div>
-                                        <a href="" class="font-medium whitespace-nowrap ml-4"><?= $row->kategori ?></a>
-                                    </div>
-                                </td>
-                                <td class="whitespace-nowrap"> <a class="flex items-center underline decoration-dotted" href="javascript:;"><?= $row->nama_brg ?></a> </td>
-                                <td class="text-center">
-                                    <div class="flex items-center">
-                                        <div class="text-xs text-slate-500 ml-1">( <?= $row->stok ?> ) </div>
-                                    </div>
-                                </td>
-                                <td class="text-center whitespace-nowrap">Rp.&nbsp;<?= number_format($row->harga, 0, ',', '.') ?>&nbsp;<small> /kg ,-</small></td>
-                                <td class="w-40">
-                                    <div class="flex items-center justify-center"><i data-lucide="dollar-sign" class="w-4 h-4 mr-2"></i><?= number_format($row->poin, 0, ',', '.') ?>&nbsp;<small>poin</small></div>
-                                </td>
-                                <td class="w-40">
+                    <?php foreach ($product as $row) : ?>
+
+                    <tr class="intro-x">
+                        <td class="!py-4">
+                            <div class="flex items-center">
+                                <div class="w-10 h-10 image-fit zoom-in">
+                                    <img alt="Midone - HTML Admin Template"
+                                        class="rounded-lg border-1 border-white shadow-md tooltip"
+                                        src="<?= base_url() . '/uploads/produk/' . $row->gambar ?>">
+                                </div>
+                                <a href="" class="font-medium whitespace-nowrap ml-4"><?= $row->kategori ?></a>
+                            </div>
+                        </td>
+                        <td class="whitespace-nowrap"> <a class="flex items-center underline decoration-dotted"
+                                href="javascript:;"><?= $row->nama_brg ?></a> </td>
+                        <td class="text-center">
+                            <div class="flex items-center">
+                                <div class="text-xs text-slate-500 ml-1">( <?= $row->stok ?> ) </div>
+                            </div>
+                        </td>
+                        <td class="text-center whitespace-nowrap">
+                            Rp.&nbsp;<?= number_format($row->harga, 0, ',', '.') ?>&nbsp;<small> /kg ,-</small></td>
+                        <td class="w-40">
+                            <div class="flex items-center justify-center"><i data-lucide="dollar-sign"
+                                    class="w-4 h-4 mr-2"></i><?= number_format($row->poin, 0, ',', '.') ?>&nbsp;<small>poin</small>
+                            </div>
+                        </td>
+                        <!-- <td class="w-40">
                                     <div class="flex items-center justify-center text-success"> <i data-lucide="check-square" class="w-4 h-4 mr-2"></i> Active </div>
-                                </td>
-                                <td class="table-report__action w-56">
-                                    <div class="flex justify-center items-center">
-                                        <a class="flex items-center mr-3" href="<?= site_url('admin/product/update/' . $row->id_brg) ?>"> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
-                                        <a class="flex items-center text-danger" href="<?= site_url('admin/product/delete/' . $row->id_brg) ?>" onclick="return confirm('Yakin Hapus')"> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
-                                    </div>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                    
-                    <?php } ?>
+                                </td> -->
+                        <td class="table-report__action w-56">
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3"
+                                    href="<?= site_url('admin/product/update/' . $row->id_brg) ?>"> <i
+                                        data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
+                                <a class="flex items-center text-danger"
+                                    href="<?= site_url('admin/product/delete/' . $row->id_brg) ?>"
+                                    onclick="return confirm('Yakin Hapus')"> <i data-lucide="trash-2"
+                                        class="w-4 h-4 mr-1"></i> Delete </a>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+
+                <?php } ?>
             </table>
         </div>
         <!-- END: Data List -->
